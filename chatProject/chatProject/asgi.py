e02b2a53084ea483f46f0b690924a0bb5e204900
sangeer-lama <sangeer.negi@gmail.com>
2023-10-11 22:44:11 +0545

    connection of channels

--- chatProject/chatProject/asgi.py
+++ chatProject/chatProject/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for chatProject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatProject.settings')
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-application = get_asgi_application()
+import room.routing
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatProject.settings')
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            room.routing.websocket_urlpatterns
+        )
+    )
+})
